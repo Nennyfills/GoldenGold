bf4db2d7d913795d887ca8cbbcb847cacf0fb10f
loveworld <eidetic>
2018-12-20 10:44:47 +0100

    addobject

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -8,6 +8,9 @@ function searchObjectListbyvalue(list, key, val){
     return list.filter(item => item[key].includes(val))
 } 
 
+function addObjecttoList(list, key, val){
+} 
+
 
 export{
     searchObjectListbyid, searchObjectListbyvalue
